c8e2104954a519630232d7ab141ea47f9074dc2c
Masculinn <hello@burakdev.com>
2025-08-12 18:59:28 +0200

    add(config): test config file added

---
+++ src/tests.config.ts
@@ -0,0 +1,8 @@
+﻿import type { TestConfigurations } from "./interfaces/index.ts";
+
+export default {
+  testEnvironment: "node",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  times: 1e3,
+} as const satisfies TestConfigurations;
