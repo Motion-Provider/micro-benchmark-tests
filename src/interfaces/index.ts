807237fa8b4d1200e91156532dfa2fc7cea13ab4
Masculinn <hello@burakdev.com>
2025-08-12 18:58:49 +0200

    add(types): global interface file added

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,10 @@
-﻿
+﻿type Times = number;
+
+export type BenchmarkFn = (label: string, times: Times, fn: () => void) => void;
+
+export interface TestConfigurations {
+  testEnvironment: string;
+  testRegex: string;
+  moduleFileExtensions: string[];
+  times: Times;
+}
