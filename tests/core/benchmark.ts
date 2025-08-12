a94ff88f42e277dda8c962aa430f1a869c3a6df6
Masculinn <hello@burakdev.com>
2025-08-12 19:00:08 +0200

    feat: mini benchmark fn added

---
+++ tests/core/benchmark.ts
@@ -0,0 +1,19 @@
+﻿import type { BenchmarkFn } from "@/interfaces/index.ts";
+import { performance } from "perf_hooks";
+
+/**
+ * Runs a given function `times` amount of times and logs the duration.
+ * @param {number} times - The amount of times to run the function.
+ * @param {function} fn - The function to run.
+ * @param {string} label - A label to display in the log.
+ */
+const benchmark: BenchmarkFn = (label, times, fn) => {
+  const start = performance.now();
+  for (let i = 0; i < times; i++) fn();
+
+  const end = performance.now();
+  const duration = (end - start).toFixed(3);
+  console.log(`${label}: ${duration}ms`);
+};
+
+export default benchmark;
--- tests/index.ts
+++ tests/index.ts
@@ -1,1 +1,8 @@
-﻿
+﻿import benchmark from "./core/benchmark.ts";
+import testsConfig from "@/tests.config.ts";
+
+const { times } = testsConfig;
+
+benchmark("init test", times, () => {
+  console.log("init test");
+});
